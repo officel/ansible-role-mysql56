e57e194c1f1efe3fc9dd50d0ceb5f671b17e474a
officel <nishimuray@gmail.com>
2016-11-07 12:41:26 +0900

    add spec for my.cnf

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -4,6 +4,13 @@ describe file('/etc/my.cnf') do
   it { should be_file }
 end
 
+describe 'MySQL config parameters' do
+  context mysql_config('character-set-server') do
+    its(:value) { should eq 'utf8' }
+  end
+end
+
+
 describe file('/var/lib/mysql') do
   it { should be_directory }
 end
